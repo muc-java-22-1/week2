bd97b4a6467c697e3e15ffb960c30677c279ddf8
Lukas <lukasduechting@mailbox.org>
2022-05-25 11:40:08 +0200

    interface & abstract class

---
+++ src/main/java/io/github/mysterix5/studentDbArrayList/MathStudent.java
@@ -0,0 +1,52 @@
+package io.github.mysterix5.studentDbArrayList;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class MathStudent implements Student {
+    final private String ID = UUID.randomUUID().toString();
+
+    private String name;
+    private int codewarsRanking;
+    private String course;
+
+    public MathStudent() {
+        name = "name";
+        codewarsRanking = 0;
+        course = "School of life";
+    }
+
+    public MathStudent(String name, int codewarsRanking, String course){
+        this.name = name;
+        this.codewarsRanking = codewarsRanking;
+        this.course = course;
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hello, i am a math student!");
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MathStudent student = (MathStudent) o;
+        return codewarsRanking == student.codewarsRanking && Objects.equals(name, student.name) && Objects.equals(course, student.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID, name, codewarsRanking, course);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student " + name + " is in " + course + " and is ranked " + codewarsRanking;
+    }
+}
